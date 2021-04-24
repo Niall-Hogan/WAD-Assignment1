3ea4df6b2650392c52aa91fe603d87b993e0e4ec
Niall_Hogan <b9013287@my.shu.ac.uk>
2021-04-24 14:25:49 +0100

    CMS update and delete done

--- Controllers/CMSController.cs
+++ Controllers/CMSController.cs
@@ -34,6 +34,69 @@ namespace WAD_Assignment1.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult UpdateRecipe(int id)
+        {
+            Recipe model = _context.Recipes.Find(id);
+            RecipeForm formModel = new RecipeForm
+            {
+                RecipeID = model.RecipeID,
+                Title = model.Title,
+                PublishDate = model.PublishDate,
+                CookingTime = model.CookingTime,
+                Step1 = model.Step1,
+                Step2 = model.Step2,
+                Step3 = model.Step3,
+                Step4 = model.Step4,
+                Ingredient1 = model.Ingredient1,
+                Ingredient2 = model.Ingredient2,
+                Ingredient3 = model.Ingredient3,
+                Ingredient4 = model.Ingredient4,
+                Ingredient5 = model.Ingredient5,
+                Ingredient6 = model.Ingredient6,
+                Ingredient7 = model.Ingredient7,
+
+            };
+            ViewBag.Image = model.Image;
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateRecipe(RecipeForm model)
+        {
+            if(ModelState.IsValid)
+            {
+                Console.WriteLine("Fired");
+                Recipe editRecipe = new Recipe
+                {
+                    RecipeID = model.RecipeID,
+                    Title = model.Title,
+                    Image = model.Image,
+                    PublishDate = model.PublishDate,
+                    CookingTime = model.CookingTime,
+                    Step1 = model.Step1,
+                    Step2 = model.Step2,
+                    Step3 = model.Step3,
+                    Step4 = model.Step4,
+                    Ingredient1 = model.Ingredient1,
+                    Ingredient2 = model.Ingredient2,
+                    Ingredient3 = model.Ingredient3,
+                    Ingredient4 = model.Ingredient4,
+                    Ingredient5 = model.Ingredient5,
+                    Ingredient6 = model.Ingredient6,
+                    Ingredient7 = model.Ingredient7,
+
+                };
+                _context.Recipes.Update(editRecipe);
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+          
+            return View(model);
+        }
+
         [HttpPost]
         public IActionResult AddRecipe(RecipeForm model)
         {
@@ -63,6 +126,21 @@ namespace WAD_Assignment1.Controllers
             _context.SaveChanges();
             return RedirectToAction(nameof(Index));
           
+        }
+
+        [HttpGet]
+        public IActionResult DeleteRecipe(int id)
+        {
+            Recipe model = _context.Recipes.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteRecipe(Recipe model)
+        {
+            _context.Recipes.Remove(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
