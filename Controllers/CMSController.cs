b66c2d110efcbb2eae4d3c13c3839964442ff391
Niall_Hogan <b9013287@my.shu.ac.uk>
2021-04-24 13:27:26 +0100

    CMS started

---
+++ Controllers/CMSController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WAD_Assignment.Models;
+using WAD_Assignment1.Models;
+
+namespace WAD_Assignment1.Controllers
+{
+    public class CMSController : Controller
+    {
+
+        private readonly ILogger<CMSController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public CMSController(ILogger<CMSController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+
+            List<Recipe> model = _context.Recipes.ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult AddRecipe()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/RecipeForm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD_Assignment1.Models
+{
+    public class RecipeForm
+    {
+    }
+}
